8781a673660282f74c262c66884856d8584b5144
Orcastinator <lorenzo.martinez.pablo@gmail.com>
2022-04-12 12:56:44 +0200

    vamos bien

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const socketio = require('socket.io');
 const app = express();
 
 // SETTINGS
-app.set('PORT', 3000);
+app.set('PORT', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
@@ -28,15 +28,105 @@ const server = app.listen(app.get('PORT'), () => {
     console.log("Server on "+app.get('PORT'));
 });
 
+// FUNCIONALIDAD SERVIDOR
+// CONSTANTES
+const W = 1500;
+const H = 700;
+const P = 50;
+const ROWS = H/P;
+const COLS = W/P;
+
+// VARIABLES GLOBALES
+var arrTo = new Array();
+var to;
+var fruits = 0;
+
+// CLASES
+class Ground{
+    constructor(x, y, state, id){
+        this.size = P;
+        this.x = x;
+        this.y = y;
+        this.id = id;
+        this.state = state;
+        this.buildable = true;
+    }
+}
+
+// ARRAY DINÁMICO
+var arrBlocks = [];
+var cont = 1;
+for (i=0;i<COLS;i++){
+    for(j=0;j<ROWS;j++){
+            arrBlocks.push(new Ground(i*P, j*P, 'ground', cont++));
+        
+    }
+}
+
+function clickBlock(b){
+    for (i=0;i<arrBlocks.length;i++){
+        if (arrBlocks[i].x === b.x && arrBlocks[i].y === b.y){
+            switch (arrBlocks[i].state){
+                case "ground":
+                    arrBlocks[i].state = "plowed";
+                    arrBlocks[i].buildable = false;
+                    io.sockets.emit('updateScreen', arrBlocks);
+                    to = setTimeout(toGround, 3000, arrBlocks[i]);
+                    arrTo[arrBlocks[i].id] = to;
+                break;
+                case "plowed":
+                    arrBlocks[i].state = "seeded";
+                    io.sockets.emit('updateScreen', arrBlocks);
+                    clearTimeout(arrTo[arrBlocks[i].id]);
+                    to = setTimeout(toGrowed, 5000, arrBlocks[i]);
+                    arrTo[arrBlocks[i].id] = to;
+                break;
+                case "growed":
+                    arrBlocks[i].state = "plowed";
+                    io.sockets.emit('updateScreen', arrBlocks);
+                    clearTimeout(arrTo[arrBlocks[i].id]);
+                    fruits++;
+                    arrBlocks[i].buildable = false;
+                    to = setTimeout(toGround, 3000, arrBlocks[i]);
+                    arrTo[arrBlocks[i].id] = to;
+                break;
+                case "dead":
+                    arrBlocks[i].state = "ground";
+                    io.sockets.emit('updateScreen', arrBlocks);
+                    clearTimeout(arrTo[arrBlocks[i].id]);
+                break;
+                default:
+            }  
+        }
+    }
+
+}
+
+// FUNCIONES TIMEOUTS
+function toGround(b){
+    b.state = "ground";
+    io.sockets.emit('updateScreen', arrBlocks);
+}
+function toGrowed(b){
+    b.state = "growed";
+    io.sockets.emit('updateScreen', arrBlocks);
+    to = setTimeout(toDead, 7000, b);
+    arrTo[b.id] = to;
+}
+function toDead(b){
+    b.state = "dead";
+    io.sockets.emit('updateScreen', arrBlocks);
+}
+
 // WEB SOCKETS 
 const io = socketio(server);
 
 io.on('connection', (socket) => {
-    console.log('New user');
+    console.log('New user -> '+socket.id);
+    io.to(socket.id).emit('drawInit', arrBlocks);
 
-    socket.on('001', (data) => {
-        console.log(data);
+    socket.on('clickBlock', (data) => {
+        clickBlock(data);
+        io.sockets.emit("updateScreen", arrBlocks);
     });
 });
-
-
--- src/public/js/events.js
+++ src/public/js/events.js
@@ -5,39 +5,77 @@ const P = 50;
 const ROWS = H/P;
 const COLS = W/P;
 
-// VARIABLES GENERALES
+// DOM
 var contenido = document.querySelector(".contenido");
-
-// CLASES
-class FetchClick{
-    constructor(x, y){
-        this.x = x;
-        this.y = y;
-        this.code = 'clickBlock';
-    }
-}
 
 // EVENT LISTENERS
 contenido.addEventListener('click', clickBlock);
 
 function clickBlock(e){
-    socket.emit("001", 'La puta que te parió');
     let x = Math.trunc(((e.pageX/P)*P) - (($(".contenido").offset().left/P)*P)); 
     let y = Math.trunc(((e.pageY/P)*P) - (($(".contenido").offset().top/P)*P));
     let relX = Math.trunc(x/P) * P;
     let relY = Math.trunc(y/P) * P; 
 
-    let obj = new FetchClick(relX, relY);
-    let objJson = JSON.stringify(obj);
-    
-    fetch('http://192.168.100.239:3000/clickBlock', {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: objJson
-    }, ).then(x => x.json()).then(y => respuesta(y));
-    
+    let obj = {
+        x: relX,
+        y: relY
+    };
+    socket.emit("clickBlock", obj);
 }
 
-function respuesta(y){
-    console.log(y);
+// SOCKETS
+
+socket.on('drawInit', (data) => {
+    drawInit(data);
+});
+
+socket.on('updateScreen', (data) => {
+    updateScreen(data);
+});
+
+// FUNCIONES CLIENTE
+function drawInit (arr){
+    for (i=0;i<arr.length;i++){
+        var aux = document.createElement('div');
+        if (arr[i].state == 'ground'){
+            console.log(arr[i].state);
+            aux.classList.add(arr[i].state);
+        }
+        else {
+            console.log(arr[i].state);
+            aux.classList.add('ground');
+            aux.classList.add(arr[i].state);
+        }
+        aux.style.left = arr[i].x + 'px';
+        aux.style.top = arr[i].y + 'px';
+        contenido.appendChild(aux);
+    }
+}
+
+function updateScreen(arr) {
+    let arrAux = Array.from(document.querySelectorAll('.ground'));
+    console.log(arrAux.length);
+    for (i=0;i<arr.length;i++){
+        let n = arrAux[i].className.lastIndexOf(" ");
+        let aux = arrAux[i].className.slice(n+1, arrAux[i].className.length); // Toma la última clase que es la que define el estado real del block
+
+        if (arr[i].state != aux){
+            if (arr[i].state == 'ground'){ // Si el terreno ha cambiado a ground, simplemente se elimina la clase específica
+                arrAux[i].classList.remove(aux);
+            }
+            else if (aux == 'ground'){ // Si el elemento pasa de ground a algo específico, simplemente se añade la nueva clase
+                arrAux[i].classList.add(arr[i].state);
+            }
+            else { // Si el elemento ya tenía una clase específica, se sustituye por la actual
+                arrAux[i].classList.replace(aux, arr[i].state);
+            }
+        }
+    }
+}
+
+function printFruits(f){
+    let h = document.getElementById('aux');
+    h.style.paddingLeft = "40px";
+    h.innerHTML = "<h2> Fruits obtained -> "+f+"</h2>";
 }
